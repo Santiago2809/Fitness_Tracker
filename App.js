4bb8dcdaabfffdcc5d3f74e72fc9a22dd11d5481
Santiago Cecena <santycc00@hotmail.com>
2024-09-22 10:41:47 -0700

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { Login } from './components/Login';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<PaperProvider>
+			<View>
+				<Login />
+				<StatusBar />
+			</View>
+		</PaperProvider>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent("Fitness Tracker", () => App);
