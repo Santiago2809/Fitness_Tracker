5bb2362f2eddbf48fd122037316a1ad9d24cc164
Santiago Cecena <santycc00@hotmail.com>
2024-09-23 13:23:04 -0700

    feat: add safe preview area

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { AppRegistry, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import { Login } from './components/Login';
-import { SafeAreaContext, SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Welcome } from './components/Auth/Welcome';
 
 export default function App() {
     return (
         <PaperProvider>
             <SafeAreaProvider>
                 <View>
-                    <Login />
+                    <Welcome />
                     <StatusBar />
                 </View>
             </SafeAreaProvider>
