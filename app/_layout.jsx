a21e0176bf0f667298003a598b9840efb7d291a8
Santiago Cecena <santycc00@hotmail.com>
2024-09-29 11:37:10 -0700

    feat: implement the observer function to handle the authentication of the user

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,24 @@
-import { Slot, Stack } from "expo-router";
+import { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router, Slot } from "expo-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { AUTH } from "../firebaseConfig";
 
 
-export default function HomeLayout() {
+export default function AppLayout() {
+
+    useEffect(() => {
+        onAuthStateChanged(AUTH, (user) => {
+            //* Si el usuario esta autenticado lo manda para la pantalla principal de home
+            if (user) {
+                router.replace("/home")
+            }
+            //* Si el usuario NO esta autenticado lo manda para la pantalla de bienvenida
+            else {
+                router.replace("/")
+            }
+        })
+    }, [])
 
     return (
         <SafeAreaView>
