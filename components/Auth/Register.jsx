bfc9164c76dc0a96db280e7ce57cf78905445144
Santiago Cecena <santycc00@hotmail.com>
2024-10-13 15:42:21 -0700

    feat: add validation and radio button group for gender

--- components/Auth/Register.jsx
+++ components/Auth/Register.jsx
@@ -1,8 +1,8 @@
 import { router } from 'expo-router';
 import { useState } from 'react';
-import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback } from 'react-native';
 import { StyleSheet, View } from 'react-native';
-import { Appbar, Button, Text } from 'react-native-paper';
+import { Appbar, Button, RadioButton, Text } from 'react-native-paper';
 import { userRegister } from '../../services/auth';
 import { Input } from '../UI/Register/Input';
 
@@ -10,20 +10,39 @@ export function Register() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [gender, setGender] = useState('');
+    const [gender, setGender] = useState('male');
     const [height, setHeight] = useState('');
     const [weight, setWeight] = useState('');
 
     const [error, setError] = useState({});
 
     function handleRegister() {
-        if (!name.match(/^[A-Za-z]{2,}$/)) {
-            setError((prev) => ({ ...prev, name: 'Ingresa un nombre valido, solo letras y mayor a 2 caracteres' }));
+        if (!name.match(/^[A-Za-z\s]{3,}$/)) {
+            setError((prev) => ({ ...prev, name: 'Ingresa un nombre valido, solo letras y mayor a 2 caracteres.' }));
+        } else {
+            setError((prev) => ({ ...prev, name: null }));
         }
-        if (email.length < 4 && password.length < 6 && name.length < 4) {
-            return;
+        if (!email.match(/^\w+@[a-z]+\.[a-z]{2,}/)) {
+            setError((prev) => ({ ...prev, email: 'Ingresa un email valido.' }));
+        } else {
+            setError((prev) => ({ ...prev, email: null }));
         }
-        // userRegister(email, password, name);
+        if (!password.match(/^(?=.*[!@#$%^&*()_+{}\[\]:;"'|\\<>,.?/~`-])[A-Za-z\d!@#$%^&*()_+{}\[\]:;"'|\\<>,.?/~`-]{6,}$/)) {
+            setError((prev) => ({ ...prev, password: 'Ingresa una contaseña valida, minimo 6 caracteres, que incluya letras numeros y un caracter especial.' }));
+        } else {
+            setError((prev) => ({ ...prev, password: null }));
+        }
+        if (!height.match(/^\d{2,3}$/)) {
+            setError((prev) => ({ ...prev, height: 'Ingresa una altura valida.' }));
+        } else {
+            setError((prev) => ({ ...prev, height: null }));
+        }
+        if (!weight.match(/^\d{2,3}$/)) {
+            setError((prev) => ({ ...prev, weight: 'Ingresa un peso valido.' }));
+        } else {
+            setError((prev) => ({ ...prev, weight: null }));
+        }
+        // userRegister({ email, password, name, gender, height, weight });
     }
 
     return (
@@ -36,35 +55,51 @@ export function Register() {
                         }}
                     />
                 </Appbar>
-                <Text variant="headlineLarge" style={style.title}>
-                    Registrarse
-                </Text>
-                <Input value={name} error={error.name} onChange={(text) => setName(text)} label="Nombre(s)" onIconPress={() => setName('')} />
-                <Input value={email} onChange={(text) => setEmail(text)} label="Correo electronico" onIconPress={() => setEmail('')} />
-                <Input value={password} onChange={(text) => setPassword(text)} label="Contraseña" onIconPress={() => setPassword('')} />
-                <Input value={gender} onChange={(text) => setGender(text)} label="Género" onIconPress={() => setGender('')} />
-                <Input value={height} onChange={(text) => setHeight(text)} label="Estatura" number={true} onIconPress={() => setHeight('')} />
-                <Input value={weight} onChange={(text) => setWeight(text)} label="Peso" number={true} onIconPress={() => setWeight('')} />
+                <ScrollView>
+                    <Text variant="headlineLarge" style={style.title}>
+                        Registrarse
+                    </Text>
+                    <Input value={name} error={error.name} onChange={(text) => setName(text.trim())} label="Nombre(s)" onIconPress={() => setName('')} />
+                    <Input value={email} error={error.email} onChange={(text) => setEmail(text.trim())} label="Correo electronico" onIconPress={() => setEmail('')} />
+                    <Input value={password} error={error.password} onChange={(text) => setPassword(text.trim())} label="Contraseña" onIconPress={() => setPassword('')} />
+                    <View className="border border-black rounded-md" style={{ marginHorizontal: 10, marginVertical: 15 }}>
+                        <RadioButton.Group onValueChange={(value) => setGender(value)} value={gender}>
+                            <RadioButton.Item color="#FF7F3E" label="Hombre" labelVariant="titleMedium" labelStyle={{ textDecorationLine: gender === 'male' ? 'underline' : 'none' }} value="male" status={gender === 'male' ? 'checked' : 'unchecked'} />
+                            <RadioButton.Item
+                                color="#FF7F3E"
+                                label="Mujer"
+                                labelVariant="titleMedium"
+                                labelStyle={{ textDecorationLine: gender === 'female' ? 'underline' : 'none' }}
+                                value="female"
+                                status={gender === 'female' ? 'checked' : 'unchecked'}
+                            />
+                        </RadioButton.Group>
+                    </View>
+                    {/* <Input value={gender} error={error.gender} onChange={(text) => setGender(text)} label="Género" onIconPress={() => setGender('')} /> */}
+                    <Input value={height} error={error.height} onChange={(text) => setHeight(text.trim())} label="Estatura" number={true} onIconPress={() => setHeight('')} />
+                    <Input value={weight} error={error.weight} onChange={(text) => setWeight(text.trim())} label="Peso" number={true} onIconPress={() => setWeight('')} />
 
-                <Button
-                    onPress={handleRegister}
-                    mode="elevated"
-                    buttonColor="#FF7F3E"
-                    textColor="white"
-                    style={{
-                        width: 200,
-                        borderRadius: 250,
-                        marginTop: 15,
-                        alignSelf: 'center',
-                    }}
-                    labelStyle={{
-                        fontSize: 20,
-                        paddingHorizontal: 10,
-                        paddingVertical: 5,
-                    }}
-                >
-                    Crear cuenta
-                </Button>
+                    <Button
+                        onPress={handleRegister}
+                        mode="elevated"
+                        buttonColor="#FF7F3E"
+                        textColor="white"
+                        style={{
+                            width: 200,
+                            borderRadius: 250,
+                            marginTop: 15,
+                            marginBottom: 50,
+                            alignSelf: 'center',
+                        }}
+                        labelStyle={{
+                            fontSize: 20,
+                            paddingHorizontal: 10,
+                            paddingVertical: 5,
+                        }}
+                    >
+                        Crear cuenta
+                    </Button>
+                </ScrollView>
             </View>
         </TouchableWithoutFeedback>
     );
