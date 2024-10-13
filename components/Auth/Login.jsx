0e85d0f4adb2795a89aa563b505177dbab124d2f
Santiago Cecena <santycc00@hotmail.com>
2024-10-13 12:26:07 -0700

    feat: add validations and use input components

--- components/Auth/Login.jsx
+++ components/Auth/Login.jsx
@@ -1,78 +1,81 @@
 import { Link, router } from 'expo-router';
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView } from 'react-native';
 import { Appbar, Button, Text } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { userLogin } from '../../services/auth';
-
 
 export const Login = () => {
     const [passwordVisible, setPasswordVisible] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
     function handleLogin() {
-        userLogin(email, password);
+        if (!email.match(/\w{3,}@[a-z]{3,}\.\D{2,}/)) {
+            setError('Por favor ingrese un email valido');
+            return;
+        }
+        if (password.length < 2) {
+            setError('Por favor ingrese una contraseña valida');
+            return;
+        }
+        const isError = userLogin(email, password);
+        isError.then().catch((error) => {
+            setError(error.message);
+        });
     }
 
-
     return (
-        <View style={styles.container}>
-            <Appbar>
-                <Appbar.BackAction onPress={() => { router.replace("/") }} />
-            </Appbar>
-            <Text style={styles.welcomeText}>¡Bienvenido de nuevo!</Text>
-
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Correo electrónico"
-                    style={styles.input}
-                    value={email}
-                    onChangeText={(text) => setEmail(text)}
-                />
-                {email.length > 0 && (
-                    <TouchableOpacity onPress={() => setEmail('')}>
-                        <Icon name="close-circle" size={24} color="#999" style={{ marginLeft: 10 }} />
-                    </TouchableOpacity>
-                )}
-            </View>
-
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Contraseña"
-                    style={styles.input}
-                    value={password}
-                    onChangeText={value => setPassword(value)}
-                    secureTextEntry={!passwordVisible}
-                />
-                <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
-                    <Icon
-                        name={passwordVisible ? "eye-off-outline" : "eye-outline"}
-                        size={24}
-                        color="#999"
-                        style={{ marginLeft: 10 }}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+                <Appbar className="bg-white" style={{ backgroundColor: 'white' }}>
+                    <Appbar.BackAction
+                        onPress={() => {
+                            router.replace('/');
+                        }}
                     />
-                </TouchableOpacity>
+                </Appbar>
+
+                <View className="mb-8">
+                    <Text style={styles.welcomeText}>¡Bienvenido de nuevo!</Text>
+                    <View style={{ minHeight: 65, width: '100%' }}>
+                        {error && (
+                            <Text variant="titleLarge" className="text-red-600 text-center pt-3">
+                                {error}
+                            </Text>
+                        )}
+                    </View>
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput placeholder="Correo electrónico" style={styles.input} value={email} onChangeText={(text) => setEmail(text)} />
+                    {email.length > 0 && (
+                        <TouchableOpacity onPress={() => setEmail('')}>
+                            <Icon name="close-circle" size={24} color="#999" style={{ marginLeft: 10 }} />
+                        </TouchableOpacity>
+                    )}
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TextInput placeholder="Contraseña" style={styles.input} value={password} onChangeText={(value) => setPassword(value)} secureTextEntry={!passwordVisible} />
+                    <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
+                        <Icon name={passwordVisible ? 'eye-off-outline' : 'eye-outline'} size={24} color="#999" style={{ marginLeft: 10 }} />
+                    </TouchableOpacity>
+                </View>
+
+                <Text style={styles.forgotPassword}>¿Has olvidado tu contraseña?</Text>
+
+                <Button mode="contained" buttonColor="#FF7F3E" textColor="white" style={styles.loginButton} onPress={handleLogin}>
+                    Iniciar sesión
+                </Button>
+
+                <Link asChild href="/auth/register">
+                    <Text style={{ textAlign: 'center', marginTop: 20 }}>
+                        ¿No tienes una cuenta? <Text style={{ color: '#FF7F3E', fontWeight: 'bold' }}>Regístrate</Text>
+                    </Text>
+                </Link>
             </View>
-
-            <Text style={styles.forgotPassword}>¿Has olvidado tu contraseña?</Text>
-
-            <Button
-                mode="contained"
-                buttonColor="#FF7F3E"
-                textColor="white"
-                style={styles.loginButton}
-                onPress={handleLogin}
-            >
-                Iniciar sesión
-            </Button>
-
-            <Link asChild href="/auth/register">
-                <Text style={{ textAlign: 'center', marginTop: 20 }}>
-                    ¿No tienes una cuenta? <Text style={{ color: '#FF7F3E', fontWeight: 'bold', }}>Regístrate</Text>
-                </Text>
-            </Link>
-        </View>
+        </TouchableWithoutFeedback>
     );
 };
 
@@ -88,7 +91,6 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         marginTop: 150,
-        marginBottom: 60,
     },
     inputContainer: {
         flexDirection: 'row',
@@ -102,7 +104,7 @@ const styles = StyleSheet.create({
     input: {
         flex: 1,
         height: 50,
-        paddingHorizontal: 10
+        paddingHorizontal: 10,
     },
     forgotPassword: {
         textAlign: 'right',
