c20d6aaad32df8ea0f4b18902a452d65cd458eba
Santiago Cecena <santycc00@hotmail.com>
2024-10-13 12:22:23 -0700

    refactor: use navbar component

--- app/home/_layout.jsx
+++ app/home/_layout.jsx
@@ -1,13 +1,12 @@
-import { Slot } from "expo-router";
-import { View } from "react-native";
-
+import { Slot } from 'expo-router';
+import { View } from 'react-native';
+import { NavBar } from '../../components/UI/Navbar';
 
 export default function HomeLayout() {
-
-
     return (
-        <View className="px-3 pt-7">
+        <View className="px-3 pt-7 h-screen relative">
             <Slot />
+            <NavBar />
         </View>
-    )
+    );
 }
