201c4b958ab1308c491e20316dd8b84c2f93a868
Santiago Cecena <santycc00@hotmail.com>
2024-10-13 12:12:58 -0700

    feat: create hook for handle the current user state

---
+++ hooks/useCurrentUser.jsx
@@ -0,0 +1,38 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { AUTH, DB } from '../firebaseConfig';
+
+export function useCurrentUser() {
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        (async () => {
+            const { email, uid } = AUTH.currentUser;
+            try {
+                const user = await getDoc(doc(DB, 'Usuaros', uid));
+                if (user.exists()) {
+                    const userData = user.data();
+                    setUser({
+                        email: email,
+                        name: userData.nombre,
+                        altura: userData.altura,
+                        peso: userData.peso,
+                    });
+                }
+            } catch (err) {
+                setError({
+                    msg: err.message,
+                    title: 'Ups! Something went wrong',
+                });
+            } finally {
+                setIsLoading(false);
+            }
+        })();
+    }, []);
+
+    return [user, isLoading, error];
+}
