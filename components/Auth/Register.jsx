f670e8c960515d2ca456ef49b4c5215ca2f36237
Santiago Cecena <santycc00@hotmail.com>
2024-10-13 12:27:23 -0700

    feat: add validations and use inputs components

--- components/Auth/Register.jsx
+++ components/Auth/Register.jsx
@@ -1,116 +1,62 @@
-import { router } from "expo-router";
-import { useState } from "react";
-import { Keyboard, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
-import { StyleSheet, View } from "react-native";
-import { Appbar, Button, Text, TextInput } from "react-native-paper";
-import { userRegister } from "../../services/auth";
-
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Appbar, Button, Text } from 'react-native-paper';
+import { userRegister } from '../../services/auth';
+import { Input } from '../UI/Register/Input';
 
 export function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [gender, setGender] = useState('');
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
 
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordVisible, setPasswordVisible] = useState(false)
-    const [gender, setGender] = useState("")
-    const [height, setHeight] = useState("")
-    const [weight, setWeight] = useState("")
+    const [error, setError] = useState({});
 
     function handleRegister() {
-        if (email.length < 4 && password.length < 6 && name.length < 4) { return }
-        userRegister(email, password, name)
+        if (!name.match(/^[A-Za-z]{2,}$/)) {
+            setError((prev) => ({ ...prev, name: 'Ingresa un nombre valido, solo letras y mayor a 2 caracteres' }));
+        }
+        if (email.length < 4 && password.length < 6 && name.length < 4) {
+            return;
+        }
+        // userRegister(email, password, name);
     }
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-
             <View style={style.container}>
                 <Appbar>
-                    <Appbar.BackAction onPress={() => { router.replace("/") }} />
+                    <Appbar.BackAction
+                        onPress={() => {
+                            router.replace('/');
+                        }}
+                    />
                 </Appbar>
                 <Text variant="headlineLarge" style={style.title}>
                     Registrarse
                 </Text>
-                <View style={[style.inputContainer, { marginTop: 10 }]}>
-                    <TextInput
-                        value={name}
-                        onChangeText={(text) => setName(text)}
-                        mode="outlined"
-                        style={style.input}
-                        placeholder="Nombre(s)"
-                        activeOutlineColor="black"
-                        right={<TextInput.Icon icon="cancel" />}
-                    />
-                </View>
-
-                <View style={style.inputContainer}>
-                    <TextInput
-                        value={email}
-                        onChangeText={(text) => setEmail(text)}
-                        mode="outlined"
-                        style={[style.input, { backgroundColor: "white" }]}
-                        placeholder="Correo electronico"
-                        activeOutlineColor="black"
-                        right={<TextInput.Icon icon="cancel" />}
-                    />
-                </View>
-
-                <View style={style.inputContainer}>
-                    <TextInput
-                        value={password}
-                        onChangeText={(text) => setPassword(text)}
-                        mode="outlined"
-                        style={[style.input, { backgroundColor: "white" }]}
-                        placeholder="Contraseña"
-                        secureTextEntry={!passwordVisible}
-                        activeOutlineColor="black"
-                        right={<TextInput.Icon icon="cancel" />}
-                    />
-                </View>
-
-                <View style={style.inputContainer}>
-                    <TextInput
-                        value={gender}
-                        onChangeText={(text) => setGender(text)}
-                        mode="outlined"
-                        style={[style.input, { backgroundColor: "white" }]}
-                        placeholder="Genero"
-                        activeOutlineColor="black"
-                        right={<TextInput.Icon icon="arrow-down-circle-outline" />}
-                    />
-                </View>
-
-                <View style={style.inputContainer}>
-                    <TextInput
-                        value={height}
-                        onChangeText={(text) => setHeight(text)}
-                        mode="outlined"
-                        style={[style.input, { backgroundColor: "white" }]}
-                        placeholder="Estatura"
-                        keyboardType="numeric"
-                        activeOutlineColor="black"
-                        right={<TextInput.Icon icon={() => <Text style={{ color: 'grey' }}>cm</Text>} />}
-                    />
-                </View>
-
-                <View style={style.inputContainer}>
-                    <TextInput
-                        value={weight}
-                        onChangeText={(text) => setWeight(text)}
-                        mode="outlined"
-                        style={[style.input, { backgroundColor: "white" }]}
-                        placeholder="Peso"
-                        activeOutlineColor="black"
-                        right={<TextInput.Icon icon={() => <Text style={{ color: 'grey' }}>kg</Text>} />}
-                    />
-                </View>
+                <Input value={name} error={error.name} onChange={(text) => setName(text)} label="Nombre(s)" onIconPress={() => setName('')} />
+                <Input value={email} onChange={(text) => setEmail(text)} label="Correo electronico" onIconPress={() => setEmail('')} />
+                <Input value={password} onChange={(text) => setPassword(text)} label="Contraseña" onIconPress={() => setPassword('')} />
+                <Input value={gender} onChange={(text) => setGender(text)} label="Género" onIconPress={() => setGender('')} />
+                <Input value={height} onChange={(text) => setHeight(text)} label="Estatura" number={true} onIconPress={() => setHeight('')} />
+                <Input value={weight} onChange={(text) => setWeight(text)} label="Peso" number={true} onIconPress={() => setWeight('')} />
 
                 <Button
                     onPress={handleRegister}
                     mode="elevated"
                     buttonColor="#FF7F3E"
                     textColor="white"
-                    style={{ width: 200, borderRadius: 250, marginTop: 15, alignSelf: 'center' }}
+                    style={{
+                        width: 200,
+                        borderRadius: 250,
+                        marginTop: 15,
+                        alignSelf: 'center',
+                    }}
                     labelStyle={{
                         fontSize: 20,
                         paddingHorizontal: 10,
@@ -121,7 +67,7 @@ export function Register() {
                 </Button>
             </View>
         </TouchableWithoutFeedback>
-    )
+    );
 }
 
 const style = StyleSheet.create({
@@ -135,23 +81,24 @@ const style = StyleSheet.create({
         backgroundColor: '#fff',
     },
     title: {
-        alignSelf: "flex-start",
+        alignSelf: 'flex-start',
         textAlign: 'left',
-        fontWeight: 'semibold'
+        fontWeight: 'semibold',
+        marginBottom: 10,
     },
     inputContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 8,
+        // borderWidth: 1,
+        // borderRadius: 8,
         paddingHorizontal: 10,
-        marginBottom: 15,
+        // marginBottom: 15,
     },
     input: {
         width: '100%',
         height: 50,
         marginVertical: 15,
-        backgroundColor: "white"
+        backgroundColor: 'white',
     },
-})
+});
