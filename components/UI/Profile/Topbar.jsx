53100463402642e3acff3afe12e2a6c21fd468aa
Santiago Cecena <santycc00@hotmail.com>
2024-10-20 15:18:17 -0700

    feat: add new prop for improved reusability

--- components/UI/Profile/Topbar.jsx
+++ components/UI/Profile/Topbar.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import { View } from 'react-native';
+import { Keyboard, ScrollView, TouchableWithoutFeedback, View } from 'react-native';
 import { IconButton, Menu, PaperProvider, Text } from 'react-native-paper';
 import { AUTH } from '../../../firebaseConfig';
 import { signOut } from 'firebase/auth';
 
-export function Topbar({ children }) {
+export function Topbar({ edit = false, children }) {
     const [menuVisible, setMenuVisible] = useState(false);
 
     const openMenu = () => setMenuVisible(true);
@@ -17,15 +17,19 @@ export function Topbar({ children }) {
 
     return (
         <PaperProvider>
-            <View className="p-4">
-                <View className="flex flex-row justify-between">
-                    <Text variant="displaySmall">Perfil</Text>
-                    <Menu visible={menuVisible} onDismiss={closeMenu} anchor={<IconButton onPress={openMenu} icon="dots-vertical" size={25} />}>
-                        <Menu.Item leadingIcon="logout-variant" onPress={handleSignOut} title="Cerrar sesión" />
-                    </Menu>
-                </View>
-                {children}
-            </View>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <ScrollView>
+                    <View className="p-4" style={{ marginBottom: 150 }}>
+                        <View className="flex flex-row justify-between">
+                            <Text variant="displaySmall">{edit ? 'Editar' : 'Perfil'}</Text>
+                            <Menu visible={menuVisible} onDismiss={closeMenu} anchor={<IconButton onPress={openMenu} icon="dots-vertical" size={25} />}>
+                                <Menu.Item leadingIcon="logout-variant" onPress={handleSignOut} title="Cerrar sesión" />
+                            </Menu>
+                        </View>
+                        {children}
+                    </View>
+                </ScrollView>
+            </TouchableWithoutFeedback>
         </PaperProvider>
     );
 }
