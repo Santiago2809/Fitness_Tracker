7e0dee96ab4e4a4f964300e030d4ed1e50a08b04
Santiago Cecena <santycc00@hotmail.com>
2024-10-20 15:12:45 -0700

    feat: implement data fetching and user validation on exercise screen

--- app/home/exercise/index.jsx
+++ app/home/exercise/index.jsx
@@ -1,41 +1,85 @@
 // import { router } from "expo-router";
 import { useState, useEffect } from 'react';
-import { View, FlatList, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-import { Text, Dialog, Button } from 'react-native-paper';
+import { View, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, Dialog, Button, ActivityIndicator, TextInput } from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { addExercise, getExercises } from '../../../services/exercise';
+import { addExercise, getExercises, deleteExercise } from '../../../services/exercise';
 
 export default function ExerciseScreen() {
-    const [exercises] = useState([
-        { id: '1', name: 'Bench Press' },
-        { id: '2', name: 'Squat' },
-        { id: '3', name: 'Press militar' },
-        { id: '4', name: 'Bicep Curl' },
-        { id: '5', name: 'Incline Bench press' },
-        { id: '6', name: 'Leg press' },
-    ]);
+    const [exercises, setExercises] = useState([]);
+    const [visible, setVisible] = useState(false);
+    const [loadingExcercise, setLoadingExcercise] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [newExercise, setNewExercise] = useState('');
+    const [confirmVisible, setConfirmVisible] = useState(false);
+    const [exerciseToDelete, setExerciseToDelete] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const showDialog = () => setVisible(true);
+    const hideDialog = () => {
+        setVisible(false);
+        setErrorMessage('');
+    };
+
+    const showConfirmDialog = (exercise) => {
+        setExerciseToDelete(exercise);
+        setConfirmVisible(true);
+    };
+    const hideConfirmDialog = () => {
+        setConfirmVisible(false);
+        setExerciseToDelete(null);
+    };
 
     useEffect(() => {
         (async () => {
-            getExercises();
+            setLoadingExcercise(true);
+            await loadExercises(); // Carga ejercicios db
+            setLoadingExcercise(false);
         })();
     }, []);
 
-    const [visible, setVisible] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [newExercise, setNewExercise] = useState('');
-
-    const showDialog = () => setVisible(true);
-    const hideDialog = () => setVisible(false);
+    const loadExercises = async () => {
+        const exercisesFromDb = await getExercises();
+        setExercises(exercisesFromDb);
+    };
 
     async function handleAddExercise() {
         setIsLoading(true);
-        const ejercicioId = await addExercise(newExercise);
-        setIsLoading(false);
-        if (ejercicioId) {
-            hideDialog();
-            setNewExercise('');
+        const exists = exercises.some((exercise) => exercise.name && exercise.name.toLowerCase().trim() === newExercise.toLowerCase().trim());
+        if (exists) {
+            setErrorMessage('El ejercicio ya existe.');
+            setIsLoading(false);
+            return;
         }
+        if (newExercise.length < 1) {
+            setErrorMessage('Por favor ingresa algo, no se permiten valores vacios');
+            setIsLoading(false);
+            return;
+        }
+        await addExercise(newExercise.trim());
+        setIsLoading(false);
+        loadExercises();
+        setNewExercise('');
+        hideDialog();
+    }
+
+    async function handleDeleteExercise() {
+        if (exerciseToDelete) {
+            const isDeleted = await deleteExercise(exerciseToDelete.ejercicioId);
+            if (isDeleted) {
+                const updatedExercises = exercises.filter((ex) => ex.ejercicioId !== exerciseToDelete.ejercicioId);
+                setExercises(updatedExercises);
+            }
+            hideConfirmDialog();
+        }
+    }
+
+    if (loadingExcercise) {
+        return (
+            <View className="h-full justify-center">
+                <ActivityIndicator animating={true} size={90} />
+            </View>
+        );
     }
 
     return (
@@ -49,27 +93,44 @@ export default function ExerciseScreen() {
 
             <FlatList
                 data={exercises}
-                renderItem={({ item }) => (
+                renderItem={({ item, index }) => (
                     <View style={styles.exerciseItem}>
-                        <Text style={styles.exerciseNumberText}>{item.id}</Text>
+                        <Text style={styles.exerciseNumberText}>{index + 1}</Text>
                         <Text style={{ flex: 1 }}>{item.name}</Text>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={() => showConfirmDialog(item)}>
                             <MaterialCommunityIcons name="trash-can-outline" size={24} color="black" />
                         </TouchableOpacity>
                     </View>
                 )}
+                style={{ maxHeight: 500 }}
             />
 
             <Dialog visible={visible} onDismiss={hideDialog}>
                 <Dialog.Title>Nuevo Ejercicio</Dialog.Title>
                 <Dialog.Content>
-                    <TextInput label="Nombre del ejercicio" value={newExercise} onChangeText={setNewExercise} placeholder="Ej: Squat" />
+                    <TextInput mode="outlined" label="Nombre del ejercicio" value={newExercise} onChangeText={setNewExercise} placeholder="Ej: Squat" />
+                    {errorMessage ? (
+                        <Text style={styles.errorText} className="text-red-500 mt-1">
+                            {errorMessage}
+                        </Text>
+                    ) : null}
                 </Dialog.Content>
                 <Dialog.Actions>
                     <Button onPress={hideDialog}>Cancelar</Button>
                     <Button onPress={handleAddExercise} disabled={isLoading}>
                         {isLoading ? 'Loading' : 'Agregar'}
                     </Button>
+                </Dialog.Actions>
+            </Dialog>
+
+            <Dialog visible={confirmVisible} onDismiss={hideConfirmDialog}>
+                <Dialog.Title>Confirmar eliminación</Dialog.Title>
+                <Dialog.Content>
+                    <Text>¿Estás seguro que deseas eliminar este ejercicio?</Text>
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={hideConfirmDialog}>Cancelar</Button>
+                    <Button onPress={handleDeleteExercise}>Eliminar</Button>
                 </Dialog.Actions>
             </Dialog>
         </View>
