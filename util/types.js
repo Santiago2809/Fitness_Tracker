56ed6f29ec963383c51d3b374cfd43414ac9f96d
Santiago Cecena <santycc00@hotmail.com>
2024-11-03 01:53:23 -0700

    feat: add routines routes

--- util/types.js
+++ util/types.js
@@ -1,8 +1,14 @@
 export const ROUTES = {
     HOME: '/home',
     EDIT: '/home/edit',
+
     LOGIN: '/auth/login',
     REGISTER: '/auth/register',
+
     EXERCISE: '/home/exercise',
+
     ROUTINES: '/home/routines',
+    ADDROUTINE: '/home/routines/add',
+    MYROUTINES: '/home/routines/personal',
+    SYSTEMROUTINES: '/home/routines/system',
 };
