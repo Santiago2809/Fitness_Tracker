231206c55e318b58d5b4b705a66b36e1930d5741
Santiago Cecena <santycc00@hotmail.com>
2024-11-03 01:55:51 -0700

    feat: add navigation to add and show routines and fix some styles

--- app/home/routines/index.jsx
+++ app/home/routines/index.jsx
@@ -1,23 +1,30 @@
 
+import { router } from 'expo-router';
 import { View, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Divider, Text } from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { ROUTES } from '../../../util/types';
 
 export default function RoutinesScreen() {
     const routines = [
         { id: '1', valor: 'Rutina buena' },
         { id: '2', valor: 'Rutina mala' },
         { id: '3', valor: 'Rutina curiosa' },
+        { id: '4', valor: 'Rutina curiosa' },
+        { id: '5', valor: 'Rutina curiosa' },
+        { id: '6', valor: 'Rutina curiosa' },
+        { id: '7', valor: 'Rutina curiosa' },
+        { id: '8', valor: 'Rutina curiosa' },
+        { id: '9', valor: 'Rutina curiosa' },
 
     ];
 
-
     return (
 
-        <View style={{ padding: 20 }}>
+        <View style={{ padding: 20, backgroundColor: "white" }}>
             <View style={styles.TitlesContainer}>
                 <Text style={{ fontSize: 24, marginRight: 20 }}>Mis rutinas</Text>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => router.navigate(ROUTES.ADDROUTINE)}>
                     <MaterialCommunityIcons name="plus-circle-outline" size={24} color="black" />
                 </TouchableOpacity>
             </View>
@@ -36,18 +43,17 @@ export default function RoutinesScreen() {
                 ItemSeparatorComponent={() => (
                     <View style={styles.separator} />
                 )}
-                style={{ maxHeight: 500, marginBottom: 20 }}
+                style={{ height: 200, marginBottom: 20 }}
             />
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => router.navigate(ROUTES.MYROUTINES)}>
                 <Text style={{ textAlign: "right", marginBottom: 20, fontWeight: "bold" }}>Mostrar todas</Text>
             </TouchableOpacity>
 
+            <Divider style={{ marginBottom: 50, marginTop: 25 }}></Divider>
+
             <View style={styles.TitlesContainer}>
                 <Text style={{ fontSize: 24, marginRight: 20 }}>Rutinas existentes</Text>
-                <TouchableOpacity>
-                    <MaterialCommunityIcons name="arrow-right" size={24} color="black" />
-                </TouchableOpacity>
             </View>
 
             <FlatList
@@ -56,15 +62,18 @@ export default function RoutinesScreen() {
                 renderItem={({ item }) => (
                     <View style={styles.rutineItem}>
                         <Text >{item.valor}</Text>
+                        <TouchableOpacity>
+                            <MaterialCommunityIcons name="dots-vertical" size={24} color="black" />
+                        </TouchableOpacity>
                     </View>
                 )}
                 ItemSeparatorComponent={() => (
                     <View style={styles.separator} />
                 )}
-                style={{ maxHeight: 500 }}
+                style={{ maxHeight: 200, marginBottom: 20 }}
             />
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => router.navigate(ROUTES.SYSTEMROUTINES)}>
                 <Text style={{ textAlign: "right", marginBottom: 20, fontWeight: "bold" }}>Mostrar todas</Text>
             </TouchableOpacity>
         </View>
@@ -74,7 +83,7 @@ export default function RoutinesScreen() {
 const styles = StyleSheet.create({
     TitlesContainer: {
         flexDirection: 'row',
-        alignItems: 'left',
+        alignItems: 'center',
         justifyContent: 'left',
 
     },
