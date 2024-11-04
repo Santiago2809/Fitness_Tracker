b4eec478283ad821ac97740872b84bf2d40c277d
Santiago Cecena <santycc00@hotmail.com>
2024-11-04 10:32:30 -0700

    feat: use layout to implement stack navigation

---
+++ app/home/routines/_layout.jsx
@@ -0,0 +1,18 @@
+import { Slot, Stack } from "expo-router";
+import { StyleSheet } from "react-native";
+
+
+
+export default function RoutinesLayout() {
+
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: false,
+            }}
+        />
+        // <>
+        //     <Slot />
+        // </>
+    )
+}
